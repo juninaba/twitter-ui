9ca8306710d017885538528c0c2c47bb3288d389
juninaba <43479094+juninaba@users.noreply.github.com>
2022-10-20 23:35:24 +0900

    create Widget

--- lib/main.dart
+++ lib/main.dart
@@ -33,33 +33,44 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
+        body: TweetTile(),
+      ),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1459136812631588864/aZrbkL_W_400x400.jpg'),
+          ),
+          SizedBox(width: 8),
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1459136812631588864/aZrbkL_W_400x400.jpg'),
-              ),
-              SizedBox(width: 8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Text('こんぶ @ Flutter大学'),
-                      SizedBox(width: 8),
-                      Text('2022/05/05')
-                    ],
-                  ),
-                  SizedBox(height: 4),
-                  Text('最高でした。'),
-                  IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border))
+                  Text('こんぶ @ Flutter大学'),
+                  SizedBox(width: 8),
+                  Text('2022/05/05')
                 ],
               ),
+              SizedBox(height: 4),
+              Text('最高でした。'),
+              IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border))
             ],
           ),
-        ),
+        ],
       ),
     );
   }
